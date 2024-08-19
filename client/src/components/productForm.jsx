5ab7b9e12c50fd02a974e8723a7627f8487506c7
norajeff06 <Nora.jeff06@gmail.com>
2024-08-19 22:26:04 +0500

    - Dockerize the app - And added the toastify

--- client/src/components/productForm.jsx
+++ client/src/components/productForm.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { createProduct, updateProduct } from '../api';
 import { Button, TextInput } from "flowbite-react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const ProductForm = ({ product, onSave }) => {
-
   const [formState, setFormState] = useState({
     name:  '',
     price:  '',
     stock:  ''
   });
-
 
   useEffect(() => {
     if (product) {
@@ -21,7 +21,6 @@ export const ProductForm = ({ product, onSave }) => {
     }
   }, [product]);
 
- 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormState(prevState => ({
@@ -36,55 +35,67 @@ export const ProductForm = ({ product, onSave }) => {
     const { name, price, stock } = formState;
 
     try {
+      let response;
       if (product) {
-        await updateProduct(product.id, { name, price, stock });
+        response = await updateProduct(product.id, { name, price, stock });
+        toast.success('Product updated successfully!'); 
       } else {
-        await createProduct({ name, price, stock });
+        response = await createProduct({ name, price, stock });
+        toast.success('Product created successfully!'); 
       }
-      onSave();
+
+  
+
+      if (response.status === 201) {
+        onSave();
+      } 
     } catch (error) {
+ 
       console.error('Failed to save product', error);
     }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col space-y-4"
-    >
-      <TextInput
-        type="text"
-        name="name"
-        value={formState.name} 
-        onChange={handleChange}
-        placeholder="Product Name"
-        required
-      />
-      <TextInput
-        type="number"
-        name="price"
-        value={formState.price} 
-        onChange={handleChange}
-        placeholder="Price"
-        required
-        min={1}
-      />
-      <TextInput
-        type="number"
-        name="stock"
-        value={formState.stock || ''} 
-        onChange={handleChange}
-        placeholder="Stock"
-        required
-        min={1}
-      />
-
-      <Button
-        type="submit"
-        className="self-center mt-4 mb-2"
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col space-y-4"
       >
-        {product ? 'Update' : 'Add'} Product
-      </Button>
-    </form>
+        <TextInput
+          type="text"
+          name="name"
+          value={formState.name} 
+          onChange={handleChange}
+          placeholder="Product Name"
+          required
+        />
+        <TextInput
+          type="number"
+          name="price"
+          value={formState.price} 
+          onChange={handleChange}
+          placeholder="Price"
+          required
+          min={1}
+        />
+        <TextInput
+          type="number"
+          name="stock"
+          value={formState.stock || ''} 
+          onChange={handleChange}
+          placeholder="Stock"
+          required
+          min={1}
+        />
+
+        <Button
+          type="submit"
+          className="self-center mt-4 mb-2"
+        >
+          {product ? 'Update' : 'Add'} Product
+        </Button>
+      </form>
+      <ToastContainer /> {/* This is required to display toast messages */}
+    </>
   );
 };
