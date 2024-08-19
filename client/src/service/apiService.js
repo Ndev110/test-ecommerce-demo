ef73ef440f41cf2fdee6c5e7cb0f47f2423a7d14
norajeff06 <Nora.jeff06@gmail.com>
2024-08-19 22:09:16 +0500

    Added the api token

--- client/src/service/apiService.js
+++ client/src/service/apiService.js
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: process.env.API_URL,
   headers: {
     "Content-Type": "application/json",
-    'X-API-TOKEN':""
+    'X-API-TOKEN':"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZWNvbW1lcmNlIn0.8mpDsNwrrKNPv3Wlm9ve6n4HW90D1reyGa8ha0PmD_4"
   },
 });
